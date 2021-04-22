911b3b8bd6e2085eac5494db0e5a9a31c4102149
karolyim <matej.karolyi@gmail.com>
2021-04-22 22:15:52 +0200

    [MAINTENANCE] project init

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Doplň k obrázkům v HTML data atribut data-kalorie
+a jako hodnotu do něj dej číslo, představující kalorickou
+hodnotu potraviny na obrázku (číslo si klidně vymysli).
+
+Zařiď, aby se při kliknutí na obrázek volala funkce,
+která vypíše kalorickou hodnotu potraviny.
+
+BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
+Tj. při klikání na obrázky se postupně sčítají kalorie.
+*/
