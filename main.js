6f6031eca5acd8ce31184ce46a844c7390ee1066
Marketa Majerova <marketamajerova.6@gmail.com>
2022-04-06 21:02:00 +0200

    [FEATURE] Showing calories after click

--- main.js
+++ main.js
@@ -9,3 +9,28 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+let ovoce = document.querySelectorAll('img');
+
+
+ovoce.forEach(function(obrazek){
+    obrazek.addEventListener('click', function() {
+
+        let popis = document.getElementById('vysledek');
+        popis.innerHTML = "Snedla jsi " + obrazek.dataset.kalorie + " na 100g potraviny";
+
+    });
+});
+
+
+
+// popis.innerHTML = "Snedla jsi " + ovoce.dataset.kalorie + " na 100g potraviny";
+
+
+// jablko.addEventListener('click', function() {
+//     popis.innerHTML = "Snedla jsi " + jablko.dataset.kalorie + " na 100g potraviny";
+// });
+
+// jablko.addEventListener('click', function() {
+//     popis.innerHTML = "Snedla jsi " + jablko.dataset.kalorie + " na 100g potraviny";
+// });
